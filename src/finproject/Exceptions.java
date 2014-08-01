46dc953b5f7112532c5b1544e77a4946d0fc2975
ksalesin <kas493@cornell.edu>
2014-08-01 16:39:53 -0400

    class to hold exceptions (might delete later)

---
+++ src/finproject/Exceptions.java
@@ -0,0 +1,33 @@
+package finproject;
+
+public class Exceptions {
+	public class NotFoundException extends Exception {
+		public NotFoundException(String message) {
+			super(message);
+		}
+		public NotFoundException() {
+			super("This village could not be found.");
+		}
+	}// end NotFoundException
+	
+	public class GraphEmptyException extends Exception {
+		public GraphEmptyException(String message) {
+			super(message);
+		}
+		public GraphEmptyException() {
+			super("This graph is empty.");
+		}
+	}// end GraphEmptyException
+	
+	public class RoadAlreadyExistsException extends Exception {
+		public RoadAlreadyExistsException(int cost, int start, int end) {
+			super("A road " + cost + " already connects villages " + start + " and " + end + ".");
+		}
+	}//end RoadAlreadyExistsException
+	
+	public class SameVillageException extends Exception {
+		public SameVillageException() {
+			super("A road cannot lead to and from the same village.");
+		}
+	}
+}
