84e3ea878e875f3f5b60af898973378807abf056
ksalesin <kas493@cornell.edu>
2014-08-01 20:33:05 -0400

    deleted commented out blocks

--- src/finproject/Village.java
+++ src/finproject/Village.java
@@ -10,21 +10,6 @@ public class Village {
 	int populationSize;
 	int outdegree;
 	int indegree;
-	//int currentPopulation = 1;
-	//int maxPopulation = 3;
-	//static int defaultPopulation;
-	
-	//Constructors
-	/*public Village ( int currentPopulation, int maxPopulation) {
-		this.currentPopulation = currentPopulation;
-		this.maxPopulation = maxPopulation;
-		this.name = ++totalVillage;
-	}//end Village constructor
-	
-	public Village (int currentPopulation) {
-		this(currentPopulation, defaultPopulation);
-	}//end Village constructor
-	*/
 	
 	public int getName() {return this.name;}	
 	public int getIndegree(){return indegree;}
